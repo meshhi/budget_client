1fec90cc48b1a1ce9ed19730697e32d361cb620c
meshi <mehhi@yandex.ru>
2023-07-16 16:43:01 +0300

    ws

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -3,36 +3,9 @@ import useWebSocket, { ReadyState } from 'react-use-websocket';
 import Button from '../UI/Button'
 
 const About = () => {
-  // const [socketUrl, setSocketUrl] = useState('wss://api-pub.bitfinex.com/ws/2');
-  const [socketUrl, setSocketUrl] = useState('wss://ws.bitstamp.net');
+  const [socketUrl, setSocketUrl] = useState('ws://127.0.0.1:8000');
 
-  // const ws = new WebSocket("wss://ws.bitstamp.net");
-
-  const apiCall = {
-    event: "bts:subscribe",
-    data: { channel: "order_book_btcusd" },
-  };
-
-  // ws.onopen = (event) => {
-  //   console.log('ws connec')
-  //   ws.send(JSON.stringify(apiCall));
-  // };
-
-  // ws.onmessage = function (event) {
-  //   console.log(event)
-  //   const json = JSON.parse(event.data);
-  //   try {
-  //     if ((json.event = "data")) {
-
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-
-  const [messageHistory, setMessageHistory] = useState([]);
-  const [connectionStatus, setConnectionStatus] = useState('Init ws state');
+  const [messageHistory, setMessageHistory] = useState();
 
   const {
     sendMessage,
@@ -48,36 +21,27 @@ const About = () => {
     //Will attempt to reconnect on all close events, such as server shutting down
     shouldReconnect: (closeEvent) => true,
     onMessage: (event) => {
-      // debugger
-      const data = JSON.parse(event.data);
-      // const message = JSON.parse(data.data)
       console.log('message received')
-      console.log(data.data)
-      if (data.data.bids) {
-        setMessageHistory(data.data.bids)
-      }
-  },
+      console.log(event.data)
+      setMessageHistory(event.data)
+    },
   });
 
   useEffect(() => {
+    sendMessage({
+      event: 'message',
+      payload: 'some '
+    })
   }, []);
 
   return(
     <div className="currency__container">
       {
-        messageHistory.map(item => {
-          return(
-            <div>${item[0]}</div>
-          )
-        })
+        messageHistory
       }
       <Button callback={() => {
-        const payload = {
-          "event":"ping",
-          "cid": 1234
-        }
-        sendMessage(JSON.stringify(apiCall));
-        // ws.send(1);
+
+        sendMessage(1);
       }}></Button>
 
     </div>
